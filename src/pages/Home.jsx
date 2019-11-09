655975e45fae2e5557f0c36775729ff999090a60
KyleRessLiere <KyleRessLiere>
2019-11-09 18:24:57 -0500

    new diversity picture

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,8 @@ export default class Home extends Component {
         
         <div className="container">
           <h1>Why Rate My Partner</h1>
-          <p>Do your group members act like apes?<br/>Do they make you wanna die?<br/>Do they make you question the future of humanity</p>
+          <p>Do your group members act like apes?<br/>Do they make you wanna die?<br/>Do they make you question the future of humanity<br/>So what can you to save others from this fate</p>
+          <ul><li><a href="/login">Rate My Partner</a></li> </ul>
           <img src={Students}></img>
         </div>
         

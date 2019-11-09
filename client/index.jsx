3f0900bae0fe9010243ce85b57e22e1423efac1a
KiiKoh <ikikomonkey@gmail.com>
2019-11-08 19:57:06 -0500

    Logins

--- client/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app/App.jsx';
-
-// load stylesheet
-require("./default.css");
-
-ReactDOM.render(<App />, document.getElementById('app'));

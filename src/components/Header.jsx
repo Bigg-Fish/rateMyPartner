71b90522987f8cffce819a046af8f82da7fedc8c
KiiKoh <ikikomonkey@gmail.com>
2019-11-09 10:47:56 -0500

    Worked on UI

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Header.css'
+
+class Header extends Component {
+    state = {}
+    render() {
+        return (
+            <div className="header">
+                <ul>
+                    <li><a href="/"><h1>Rate My Partner</h1></a></li>
+                    <li><a href="/secret">Secret</a></li>
+                    <li><a href="/login">Login</a></li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Header from '../components/Header'
+
+export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: 'Loading...'
+    }
+  }
+
+  componentDidMount() {
+    fetch('/api/home')
+      .then(res => res.text())
+      .then(res => this.setState({ message: res }));
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>Home</h1>
+        <p>{this.state.message}</p>
+      </div>
+    );
+  }
+}

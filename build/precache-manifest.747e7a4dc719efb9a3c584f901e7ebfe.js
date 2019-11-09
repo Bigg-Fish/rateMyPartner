c208188f1fbcfe5e6a0459f1101cf71b97392655
KiiKoh <ikikomonkey@gmail.com>
2019-11-09 15:30:58 -0500

    Added profile render

---
+++ build/precache-manifest.747e7a4dc719efb9a3c584f901e7ebfe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7e880e3e6e68a139319",
+    "url": "/static/css/main.a2eed156.chunk.css"
+  },
+  {
+    "revision": "a7e880e3e6e68a139319",
+    "url": "/static/js/main.a7e880e3.chunk.js"
+  },
+  {
+    "revision": "02f28d87cdcb919f0e79",
+    "url": "/static/js/1.02f28d87.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "350c02ad3479650308e381b32e859b05",
+    "url": "/index.html"
+  }
+];
--- src/pages/Partner.jsx
+++ src/pages/Partner.jsx
@@ -1,27 +1,63 @@
 import React, { Component } from 'react';
 import Header from "../components/Header"
-
+import defaultImg from "../defaultImg.jpg"
+import "./Partner.css"
 
 class Partner extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            partner: {
+                "userID": "ObjectId(5dc6e88828df2e0f809a5a49)",
+                "fname": "dibs",
+                "lname": "dibsy",
+                "email": "rubber.duckie@marist.edu",
+                "password": "bubblebath",
+                "ratings": [
+                    {
+                        "rater": "shark",
+                        "rating": 10,
+                        "comment": "wow"
+                    },
+                    {
+                        "rater": "rubber",
+                        "rating": 7,
+                        "comment": "okay "
+                    }
+                ]
+            }
+        }
     }
 
     componentDidMount() {
-        fetch('/api/partners/')
-            .then(res => res.text())
-            .then(res => this.setState({ partners: res }));
+        // fetch('/api/partner/')
+        //     .then(res => res.text())
+        //     .then(res => this.setState({ partner: res }));
     }
 
-    getStars(number) {
+    getStars(numStars) {
         return (
-            <div>
+            <span>
                 {
-                    Array.apply(null, { length: Math.floor(number) }).map((e, i) => (
+                    Array.apply(null, { length: numStars }).map((e, i) => (
                         <span key={i}>⭐</span>
                     ))
-                }
+
+                }{numStars}
+            </span>
+        )
+    }
+
+    getRating(partner) {
+        let ratings = partner.ratings;
+        let sum = 0;
+        for (let rating of ratings) {
+            sum += rating.rating;
+        }
+        let avg = sum / ratings.length;
+        return (
+            <div className="ratingBar">
+                {this.getStars(avg)}
             </div>
         )
     }
@@ -30,7 +66,25 @@ class Partner extends Component {
         return (
             <div>
                 <Header />
-                {this.getStars(7)}
+                <span className="profile">
+                    <img src={defaultImg} alt="Profile Pic" className="profilePic" width="150px"></img>
+                    <div>
+                        <h3>{this.state.partner.fname} {this.state.partner.lname}'s Profile</h3>
+                        {this.getRating(this.state.partner)}
+                    </div>
+                    <div className="separator"></div>
+                </span>
+                <div className="ratingsHolder">
+                    {
+                        this.state.partner.ratings.map((rating, i) =>
+                            <div className="rating" key={i}>
+                                <h1>{rating.rater}{this.getStars(rating.rating)}</h1>
+                                <p>{rating.comment}</p>
+                                <div className="separator"></div>
+                            </div>
+                        )
+                    }
+                </div>
             </div>
         );
     }

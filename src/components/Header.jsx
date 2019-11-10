65fcb901af2105f468afc5a272930eb916dc3710
KiiKoh <ikikomonkey@gmail.com>
2019-11-10 13:06:40 -0500

    Stuff

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,7 @@ class Header extends Component {
             <div className="header">
                 <ul>
                     <li><a href="/"><h1>Rate My Partner</h1></a></li>
-                    <li><a href="/secret">Secret</a></li>
-                    <li><a href="/partner/325235">Partner</a></li>
+                    <li><a href="/directory">Partner</a></li>
                     <li><a href="/login">Login</a></li>
                 </ul>
             </div>

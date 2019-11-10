dcb0e36ef6b3d09ea84d30d3fab5fe2f6a8a385f
KiiKoh <ikikomonkey@gmail.com>
2019-11-10 12:52:38 -0500

    Added directory

---
+++ src/pages/Directory.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import Header from "../components/Header"
+
+class Directory extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            partners: [
+                {
+                    id: 100001,
+                    name: "Kyle DePace"
+                },
+                {
+                    id: 100002,
+                    name: "Jacob Butchko"
+                },
+                {
+                    id: 100003,
+                    name: "Luke Jacobs"
+                },
+                {
+                    id: 100004,
+                    name: "Kyle Ress"
+                },
+                {
+                    id: 100005,
+                    name: "Kyle Liere"
+                }
+            ]
+        }
+    }
+    render() {
+        return (
+            <div>
+                <Header />
+                <h1>Viewing All {this.state.partners.length} Partners</h1>
+                <div className="partnerHolder">
+                    {
+                        this.state.partners.map((rating, i) =>
+                            <div className="rating" key={i}>
+                                <h2>{rating.rater}  {this.getStars(rating.rating)}</h2>
+                                <p>{rating.comment}</p>
+                                <div className="separator"></div>
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Directory;

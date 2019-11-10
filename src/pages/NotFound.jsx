ab9c5c2fddbc3029131215fcb497de202aa51b34
KyleRessLiere <KyleRessLiere>
2019-11-10 13:21:59 -0500

    yum

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import './NotFound.jsx'
 
 class NotFound extends Component {
     state = {}
     render() {
         
         return (<div>
-            <>
+            <p>Error 404</p>
 
 
 
